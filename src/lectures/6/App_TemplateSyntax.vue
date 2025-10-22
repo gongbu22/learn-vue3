a982dacd35e1d764d8556bdbc46aab3e5b364060
Gongbu22 <pyujin0711@naver.com>
2025-10-23 00:19:32 +0900

    init

---
+++ src/lectures/6/App_TemplateSyntax.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <h2>보간법</h2>
+    <p>{{ message }}</p>
+    <p v-once>{{ message }}</p>
+    <button v-on:click="message = message + '!!'">click!</button>
+    <hr>
+    <h2>HTML</h2>
+    <p>{{ rawHtml }}</p>
+    <p v-html="rawHtml"></p>
+    <h2>속성 바인딩</h2>
+    <div v-bind:title="dynamicTitle">마우스를 올려보세요.</div>
+    <input type="text" value="홍길동" v-bind:disabled="isInputDisabled">
+    <input v-bind="attrs">
+    <h2>Javascript</h2>
+    {{ message.split('').reverse().join('') }} <br>
+    {{ isInputDisabled ? "yes" : "no" }}
+
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  setup () {
+    const message = ref('안녕하세요');
+    const rawHtml = ref('<strong>안녕안녕</strong>');
+
+    const dynamicTitle = ref('헬로:)');
+    const isInputDisabled = ref(false);
+    const attrs = ref({
+      type: 'text',
+      value: '123455667',
+      disabled: true
+    })
+
+    return {
+      message,
+      rawHtml,
+      dynamicTitle,
+      isInputDisabled,
+      attrs,
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/lectures/7/App_readonly.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+import { reactive, readonly } from 'vue';
+
+export default {
+  setup () {
+    const original = reactive({
+      count: 0,
+    });
+    const copy = readonly(original);
+    original.count++;
+    copy.count++;
+    console.log(original.count);
+    console.log(copy.count);
+
+    // readonly() 는 그 객체를 직접 수정하는 행위만 막고
+    // 원본의 변화까지 차단하지는 않음
+
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
