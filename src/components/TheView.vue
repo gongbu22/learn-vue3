bf64017fa2575b9f62922cb28d158fccd8ac5d59
Gongbu22 <pyujin0711@naver.com>
2025-10-27 23:52:40 +0900

    ui components

---
+++ src/components/TheView.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <main>
+      <div class="container py-4">
+        <div class="container text-center">
+          <div class="row g-3">
+            <div class="col col-4">
+              <AppCard></AppCard>
+              <!-- <app-card></app-card> -->
+            </div>
+            <div class="col col-4">
+              <AppCard></AppCard>
+            </div>
+            <div class="col col-4">
+              <AppCard></AppCard>
+            </div>
+            <div class="col col-4">
+              <AppCard></AppCard>
+            </div>
+            <div class="col col-4">
+              <AppCard></AppCard>
+            </div>
+            <div class="col col-4">
+              <AppCard></AppCard>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+import App from '@/App.vue';
+import AppCard from './AppCard.vue';
+export default {
+  components: {
+    AppCard,
+  },
+  setup() {
+
+
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
--- src/main.js
+++ src/main.js
@@ -2,5 +2,12 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// AppCard 를 전역적으로 사용하게 설정
+//import AppCard from './components/AppCard.vue'
+
+const app = createApp(App)
+
+// app.component('AppCard', AppCard)
+
+app.mount('#app')
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
